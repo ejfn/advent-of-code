6e65a3ef1b290e4f2457531192168d8d37d8a352
ericvan76 <ericvan76@gmail.com>
2021-12-11 22:35:12 +1030

    day 4 numpy

--- 2021/4/day4.py
+++ 2021/4/day4.py
@@ -1,51 +1,41 @@
 import os
 import sys
-import itertools
+import numpy as np
 
 with open(os.path.join(sys.path[0], 'input.txt'), 'r') as f:
     lines = list(filter(lambda s: len(s) > 0, [line.strip() for line in f]))
 
-draw_numbers = [int(i) for i in lines[0].split(',')]
-boards = [[[(int(k), False) for k in j.split()] for j in lines[i:i+5]]
-          for i in range(1, len(lines)-1, 5)]
+draw_numbers = np.fromstring(lines[0], dtype=int, sep=',')
+boards = np.array([np.fromstring(j, dtype=int, sep=' ')
+                   for j in filter(lambda i: i != '', lines[1:])]
+                  ).reshape([-1, 5, 5])
 
 
-def is_complete(board):
-    for i in range(5):
-        if all([board[i][j][1] for j in range(5)]) or all([board[j][i][1] for j in range(5)]):
-            return True
+def is_complete(board: np.array):
+    chk = board == -1
+    if np.any(np.all(chk, axis=0)) or np.any(np.all(chk, axis=1)):
+        return True
     return False
-
-
-def sum_board(board):
-    return sum([i[0] if not i[1] else 0 for i in itertools.chain(*board)])
-
-
-def draw(board, n):
-    for i in range(5):
-        for j in range(5):
-            if board[i][j][0] == n:
-                board[i][j] = (n, True)
 
 
 def part1():
     for n in draw_numbers:
         for board in boards:
-            draw(board, n)
+            board[board == n] = -1
             if is_complete(board):
-                return sum_board(board) * n
+                return board[board != -1].sum() * n
 
 
 def part2():
     tmp = [*boards]
     for n in draw_numbers:
         for board in tmp:
-            draw(board, n)
+            board[board == n] = -1
             if is_complete(board):
                 win = board
                 win_n = n
         tmp = list(filter(lambda b: not is_complete(b), tmp))
-    return sum_board(win) * win_n
+    return win[win != -1].sum() * win_n
 
 
 print(part1())  # 49686

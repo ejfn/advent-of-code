b051ffe9a7a0f75f97c6c4208ff7a29b0446b5c5
ericvan76 <ericvan76@gmail.com>
2021-12-21 00:12:27 +1030

    day 20

---
+++ 2021/20/day20.py
@@ -0,0 +1,51 @@
+from functools import reduce
+import os
+import sys
+import numpy as np
+
+with open(os.path.join(sys.path[0], 'input.txt'), 'r') as f:
+    lines = f.readlines()
+    algorithm = [1 if c == '#' else 0 for c in lines[0].strip()]
+    input = np.array([[1 if c == '#' else 0 for c in line.strip()]
+                     for line in lines[2:]])
+
+
+def resolve(i: int, j: int, arr: np.array):
+    bit_arr_reverse = [
+        arr[i+1, j+1], arr[i+1, j], arr[i+1, j-1],
+        arr[i, j+1], arr[i, j], arr[i, j-1],
+        arr[i-1, j+1], arr[i-1, j], arr[i-1, j-1]
+    ]
+    val = reduce(lambda x, y: x+y,
+                 [x * 2 ** i for i, x in enumerate(bit_arr_reverse)])
+    return algorithm[val]
+
+
+def apply(input: np.array):
+    output = np.zeros(input.shape, dtype=int)
+    m, n = output.shape
+    for x in range(1, m-1):
+        for y in range(1, n-1):
+            output[x, y] = resolve(x, y, input)
+    return output
+
+
+def apply_twice(input: np.array):
+    x = np.pad(input, ((4, 4), (4, 4)), 'constant')  # padding
+    x = apply(apply(x))  # apply twice let infinites go zero
+    return x[2:-2, 2:-2]  # unpadding non-zero marigins
+
+
+def part1():
+    return np.sum(apply_twice(input))
+
+
+def part2():
+    x = input
+    for _ in range(25):
+        x = apply_twice(x)
+    return np.sum(x)
+
+
+print(part1())  # 5275
+print(part2())  # 16482

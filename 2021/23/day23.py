0bcc97d98e09e55c2bfe1cf37f681d911cf547c5
ericvan76 <ericvan76@gmail.com>
2021-12-25 20:37:31 +1030

    day 23

---
+++ 2021/23/day23.py
@@ -0,0 +1,148 @@
+import itertools
+from collections import defaultdict
+from heapq import heappop, heappush
+
+
+hallway = [0, 1, 3, 5, 7, 9, 10]
+rooms = {2: 'A', 4: 'B', 6: 'C', 8: 'D'}
+costs = {'A': 1, 'B': 10, 'C': 100, 'D': 1000}
+room_capacity = 2
+
+
+class State:
+    def __init__(self, init) -> None:
+        self.state = defaultdict(lambda: [])
+        for i in init:
+            self.state[i] = init[i].copy()
+
+    def __key__(self):
+        return ';'.join([str(i) + '=' + ''.join(self.state[i]) for i in range(11) if len(self.state[i]) > 0])
+
+    def __hash__(self):
+        return hash(self.__key__())
+
+    def __eq__(self, other):
+        if isinstance(other, State):
+            return self.__key__() == other.__key__()
+        return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(other, State):
+            return self.__key__() < other.__key__()
+        return NotImplemented
+
+    def print_hallway(self):
+        h = ''.join('.' if i in rooms else self.state[i][-1]
+                    if len(self.state[i]) > 0 else '.' for i in range(11))
+        print('#' + h + '#')
+
+    def print_room(self, i):
+        r = '#'.join(self.state[r][i] if len(
+            self.state[r]) > i else '.' for r in rooms)
+        if i == room_capacity - 1:
+            print('###' + r + '###')
+        else:
+            print('  #' + r + '#  ')
+
+    def print(self):
+        print('#############')
+        self.print_hallway()
+        for i in range(room_capacity, 0, -1):
+            self.print_room(i - 1)
+        print('  #########  ')
+
+    def is_available(self, a: str, pos: int):
+        if pos in rooms:
+            return a == rooms[pos] and len(self.state[pos]) < room_capacity and all([a == x for x in self.state[pos]])
+        else:
+            return len(self.state[pos]) == 0
+
+    def not_blocked(self, start, end) -> bool:
+        s = min(start, end)
+        d = max(start, end)
+        for i in range(s+1, d):
+            if i in rooms:
+                continue
+            elif len(self.state[i]) > 0:
+                return False
+        return True
+
+    def find_next_moves(self):
+        for s, e in itertools.product(range(11), repeat=2):
+            if len(self.state[s]) == 0 or s == e or (s in hallway and e in hallway):
+                continue
+            items = self.state[s]
+            a = items[-1]
+            if self.is_available(a, e) and self.not_blocked(s, e):
+                cp = State(self.state)
+                q = cp.state[s].pop()
+                cp.state[e].append(q)
+                yield (cp, cp.cost(a, e, s))
+
+    def cost(self, amph, start, end):
+        steps = abs(start - end)
+        if start in rooms:
+            steps += room_capacity - len(self.state[start]) + 1
+        if end in rooms:
+            steps += room_capacity - len(self.state[end])
+        return steps * costs[amph]
+
+
+def dijkstra(start: State, end: State):
+    solved, unsolved, hpq = dict(), dict(), []
+    unsolved[start] = 0
+    heappush(hpq, (0, start))
+    while hpq:
+        cost, k = heappop(hpq)
+        if k in solved:
+            continue
+        solved[k] = unsolved[k]
+        del unsolved[k]
+        if k == end:
+            return cost
+        for adj, cost in k.find_next_moves():
+            if adj in solved:
+                continue
+            acc_cost = solved[k] + cost
+            if adj not in unsolved or acc_cost < unsolved[adj]:
+                unsolved[adj] = acc_cost
+                heappush(hpq, (acc_cost, adj))
+    return float('inf')
+
+
+def part1():
+    initial = State({
+        2: ['B', 'D'],
+        4: ['C', 'D'],
+        6: ['A', 'B'],
+        8: ['C', 'A']
+    })
+    target = State({
+        2: ['A', 'A'],
+        4: ['B', 'B'],
+        6: ['C', 'C'],
+        8: ['D', 'D']
+    })
+    print(dijkstra(initial, target))
+
+
+def part2():
+    global room_capacity
+    room_capacity = 4
+    initial = State({
+        2: ['B', 'D', 'D', 'D'],
+        4: ['C', 'B', 'C', 'D'],
+        6: ['A', 'A', 'B', 'B'],
+        8: ['C', 'C', 'A', 'A']
+    })
+    target = State({
+        2: ['A', 'A', 'A', 'A'],
+        4: ['B', 'B', 'B', 'B'],
+        6: ['C', 'C', 'C', 'C'],
+        8: ['D', 'D', 'D', 'D']
+    })
+    print(dijkstra(initial, target))
+
+
+part1()  # 16244
+part2()  # 43226
